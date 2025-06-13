34ffaf46b9180ecbbf85869f6950fd133c530fa3
tmp-lemon <fnleonar@protonmail.com>
2025-06-13 00:27:27 -0300

    add game loop

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import pygame
 
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
